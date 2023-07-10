13590fc474c71af4e5f2610988bcaa01d280526a
Jitiwat2513 <139094628+Jitiwat2513@users.noreply.github.com>
2023-07-10 10:40:37 +0700

    Add files via upload

---
+++ Wlord.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{	cout <<"Student Code : 6606021620228"<<endl;
+	cout <<"Student Name : Jitiwat Phajai-on"<<endl;
+	cout <<"    Division : Information Technology"<<endl;
+	cout <<"        Year : 1"<<endl;
+	cout <<"        Room : RC"<<endl;
+	cout <<"         Sex : Male"<<endl;
+	cout <<"         Age : 20"<<endl;
+	system("pause");
+	return 0;
+}
